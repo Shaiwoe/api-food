5ffae728ba4361299515286e0a8fdd75898dcbe7
Mohsen <shaiwoe@gmail.com>
2023-05-13 23:57:22 +0330

    return sms

--- app/Channels/SmsChannel.php
+++ app/Channels/SmsChannel.php
@@ -9,7 +9,7 @@ class SmsChannel
 {
     public function send($notifiable, Notification $notification)
     {
-
+        return 'Done';
 
         $receptor = $notifiable->cellphone;
         $template = "otp";
